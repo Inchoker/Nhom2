063e8e1d5fd6743bd260b6c1688ee453653790a2
vanba278 <nguyenvanbapc66@gmail.com>
2019-10-29 20:53:16 +0700

    initial commit

---
+++ genre.js
@@ -0,0 +1,30 @@
+//Play youtube in modal style
+$(document).ready(function() {
+
+    // Gets the video src from the data-id on each img tag
+
+    let $videoSrc;
+    // $('.vungChon').click(function() {
+    //     $videoSrc = $(this).data("id");
+    // });
+    // console.log($videoSrc);
+    $(document).on('click', '.vungChon', function(){
+        $videoSrc = $(this).data('id');
+    });
+
+
+    // when the modal is opened autoplay it  
+    $('#myModal').on('shown.bs.modal', function(e) {
+
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', "https://www.youtube.com/embed/" + $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+
+
+
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function(e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
+    })
+})
