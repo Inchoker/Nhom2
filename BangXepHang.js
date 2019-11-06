c48ffec8bf889a6827971c1679c27dc79ea981ac
Le Huu Tri <lehuutri1208@gmail.com>
2019-11-06 19:34:57 +0700

    trilh commit

--- BangXepHang.js
+++ BangXepHang.js
@@ -33,7 +33,7 @@ function showTrend(json) {
         s += `<!-- video thumbnail  -->
         <div class="videothumbnail">
             <!-- add tooltip -->
-            <span data-toggle="tooltip" data-placement="right" title="Click to listen on youtube">
+            <span data-toggle="tooltip" data-placement="top" title="Click to listen on youtube">
 
                 <!-- an vao toggle ra modal  -->
                 <!-- them id vao the img  -->
@@ -80,20 +80,31 @@ $(window).on("load", function() {
 
     })
 
-    $(document).on("click", "#popUp", function() {
-        $(".pUCV").fadeOut();
-        localStorage.setItem("clicked", 1)
-    })
-
-
-
-
-
-
-    // document ready  
 });
 
-//neu clicked =1 thi k show popup
-if (localStorage.getItem("clicked") == 1) {
-    document.getElementsByClassName("pUCV")[0].style.display = "none";
+// function cua button to top 
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//function back
+
+function goBack() {
+    window.history.back();
 }
--- index.js
+++ index.js
@@ -83,11 +83,23 @@ function getEmoMusic(json) {
     return videoId;
 }
 
+//phuong thuc cua popup
+//neu clicked =1 thi k show popup
+$(document).ready(function() {
+    if (localStorage.getItem("clicked") == 1) {
+        document.getElementsByClassName("pUCV")[0].style.display = "none";
+    }
+    $(document).on("click", "#popUp", function() {
+        $(".pUCV").fadeOut();
+        localStorage.setItem("clicked", 1)
+    })
+})
 
 
 //function bat modal
 function batModal(id) {
-    // when the modal is opened autoplay it  
+    $("#myModal").modal()
+        // when the modal is opened autoplay it  
     $('#myModal').on('shown.bs.modal', function(e) {
 
         // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
@@ -140,4 +152,25 @@ function emo7() {
 
 function emo8() {
     showFunEmo(listEmo[7], batModal);
+}
+
+//function cua button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
